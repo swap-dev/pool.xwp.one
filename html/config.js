9d97cb421922a4fa3438849f91d275fe4831193e
sebseb7 <sebgreenbus@gmail.com>
2020-01-05 11:27:48 +0000

    genesis

---
+++ html/config.js
@@ -0,0 +1,20 @@
+var api = "/rpc";
+
+var poolHost = "pool.xwp.one";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+
+var guiWallet = "https://github.com/swap-dev/swap-gui/releases";
+
+var paperWallet = "freehavenaddr.html";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+var blockchainExplorer = "https://explorer.xwp.one/block/{id}";
+
+var transactionExplorer = "https://explorer.xwp.one/tx/{id}";
---
+++ html_test/config.js
@@ -0,0 +1,20 @@
+var api = "/rpc";
+
+var poolHost = "pool.test.xwp.one";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+
+var guiWallet = "https://github.com/swap-dev/swap-gui/releases";
+
+var paperWallet = "freehavenaddr.html";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+var blockchainExplorer = "https://explorer.test.xwp.one/block/{id}";
+
+var transactionExplorer = "https://explorer.test.xwp.one/tx/{id}";
